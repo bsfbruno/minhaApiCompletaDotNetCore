1ae0fb11d7b96e8e85e8bb2c17ec1aa92dfc30d9
bsfbruno <brunosuportefs@gmail.com>
2021-11-19 16:44:01 -0300

    Implementando o JWT

---
+++ src/DevIO.Api/Extension/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Extension
+{
+    public class AppSettings
+    {
+        public String Secret {get; set;}
+        public int ExpiracaoHoras {get; set;}
+        public String Emissor {get; set;}
+        public String ValidoEm {get; set;}
+    }
+}

5253fde64ca4f3789d9ead61647ba444cb6635e7
bsfbruno <brunosuportefs@gmail.com>
2021-11-24 15:58:35 -0300

    Usando JWT no Swagger

--- src/DevIO.Api/Configuration/SwaggerConfig.cs
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -16,11 +16,70 @@ namespace DevIO.Api.Configuration
 {
     public static class SwaggerConfig
     {
+        //public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        //{
+        //    services.AddSwaggerGen(c =>
+        //    {
+        //        c.OperationFilter<SwaggerDefaultValues>();
+
+        //        var securityDefinition = new OpenApiSecurityScheme
+        //        {
+        //            Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+        //            Name = "Authorization",
+        //            In = ParameterLocation.Header,
+        //            Type = SecuritySchemeType.ApiKey,
+        //            Scheme = "Bearer"
+        //        };
+
+        //        var security = new OpenApiSecurityRequirement()
+        //        {
+        //            {securityDefinition, new string[] { }}
+        //        };
+
+        //        c.AddSecurityDefinition("Bearer", securityDefinition);
+
+        //        c.AddSecurityRequirement(security);
+        //    });
+
+        //    return services;
+        //}
+
         public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
             {
                 c.OperationFilter<SwaggerDefaultValues>();
+
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }}
+                };
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        }, new List<string>()
+                    }
+                });
             });
 
             return services;
